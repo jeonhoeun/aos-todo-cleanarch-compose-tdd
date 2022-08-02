593905706d0af567b0c51a77ee27faf21d6b9244
jeonhoeun <jeonhoeun@naver.com>
2022-08-02 16:43:06 +0900

    compose 설정 추가

---
+++ app/src/main/java/com/riyusoft/todo/LauncherActivity.kt
@@ -0,0 +1,27 @@
+package com.riyusoft.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.riyusoft.myapplication.ui.theme.TodoTheme
+
+class LauncherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TodoTheme {
+                Text("test")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun preview(){
+    Text("test")
+}
